b45132927c93a5130c38bc2a9ea9d4a1b2ec3ee8
Zvonko Kaiser <zkaiser@nvidia.com>
2025-08-11 18:21:45 +0000

    src: Some minor cleanups
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

---
+++ src/attach.rs
@@ -0,0 +1,81 @@
+use crate::daemon::Action;
+use crate::kata_agent;
+use crate::kmsg;
+use crate::ndev;
+use crate::nvrc::NVRC;
+use log::{debug, error};
+use nix::unistd::{fork, ForkResult};
+use std::sync::mpsc;
+use std::thread::sleep;
+use std::time::Duration;
+
+use anyhow::{Context, Result};
+
+impl NVRC {
+    pub fn cold_plug(&mut self) -> Result<()> {
+        debug!("cold-plug mode");
+        self.setup_gpu();
+        match unsafe { fork() }.expect("fork cold-plug") {
+            ForkResult::Parent { .. } => {
+                kata_agent().context("kata-agent cold-plug parent")?;
+            }
+            ForkResult::Child => loop {
+                sleep(Duration::from_secs(1));
+                if let Err(e) = self.poll_syslog() {
+                    error!("poll syslog: {e}");
+                    break;
+                }
+            },
+        }
+        Ok(())
+    }
+
+    pub fn manage_daemons(&mut self, action: Action) -> Result<()> {
+        for f in [
+            NVRC::nvidia_persistenced,
+            NVRC::nv_hostengine,
+            NVRC::dcgm_exporter,
+        ] {
+            f(self, action.clone())?;
+        }
+        Ok(())
+    }
+
+    pub fn hot_plug(&mut self) -> Result<()> {
+        debug!("hot-plug mode");
+        match unsafe { fork() }.expect("fork hot-plug") {
+            ForkResult::Parent { .. } => {
+                kata_agent().context("kata-agent hot-plug parent")?;
+            }
+            ForkResult::Child => {
+                self.handle_hot_plug_events().context("hot-plug events")?;
+            }
+        }
+        Ok(())
+    }
+
+    fn handle_hot_plug_events(&mut self) -> Result<()> {
+        let (tx, rx) = mpsc::channel::<&str>();
+        ndev::udev(tx.clone());
+        kmsg::watch_for_pattern("NVRM: Attempting to remove device", tx.clone());
+        for ev in rx {
+            debug!("event: {ev}");
+            match ev {
+                "hot-plug" => {
+                    self.get_nvidia_devices(None)?;
+                    self.setup_gpu();
+                }
+                "hot-unplug" => {
+                    self.manage_daemons(Action::Stop)?;
+                    sleep(Duration::from_millis(3000));
+                    self.get_nvidia_devices(None)?;
+                    if !self.nvidia_devices.is_empty() {
+                        self.manage_daemons(Action::Start)?;
+                    }
+                }
+                _ => {}
+            }
+        }
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-use anyhow::Result;
-use nix::unistd::{fork, ForkResult};
-use std::sync::mpsc;
-use std::thread::sleep;
-use std::time::Duration;
-
-mod check_supported;
-mod container_toolkit;
+mod attach;
 mod coreutils;
 mod cpu;
 mod daemon;
@@ -19,7 +12,9 @@ mod mount;
 mod ndev;
 mod nvrc;
 mod pci_ids;
+mod supported;
 mod syslog;
+mod toolkit;
 mod user_group;
 
 #[macro_use]
@@ -39,10 +34,10 @@ macro_rules! must {
     };
 }
 
-use container_toolkit::{nvidia_ctk_cdi, nvidia_ctk_system};
 use daemon::Action;
 use kata_agent::kata_agent;
 use nvrc::NVRC;
+use toolkit::{nvidia_ctk_cdi, nvidia_ctk_system};
 
 fn main() {
     lockdown::set_panic_hook();
@@ -58,75 +53,12 @@ fn main() {
     #[cfg(feature = "confidential")]
     must!(init.query_cpu_cc_mode());
     must!(init.get_nvidia_devices(None));
-    init.hot_or_cold_plug.get(&init.cold_plug).unwrap()(&mut init);
+    must!(init.hot_or_cold_plug.get(&init.cold_plug).unwrap()(
+        &mut init
+    ));
 }
 
 impl NVRC {
-    fn manage_daemons(&mut self, action: Action) -> Result<()> {
-        for f in [
-            NVRC::nvidia_persistenced,
-            NVRC::nv_hostengine,
-            NVRC::dcgm_exporter,
-        ] {
-            f(self, action.clone())?;
-        }
-        Ok(())
-    }
-
-    fn cold_plug(&mut self) {
-        debug!("cold-plug mode");
-        self.setup_gpu();
-        match unsafe { fork() }.expect("fork cold-plug") {
-            ForkResult::Parent { .. } => {
-                kata_agent().expect("kata-agent cold-plug parent");
-            }
-            ForkResult::Child => loop {
-                sleep(Duration::from_secs(1));
-                if let Err(e) = self.poll_syslog() {
-                    error!("poll syslog: {e}");
-                    break;
-                }
-            },
-        }
-    }
-
-    fn hot_plug(&mut self) {
-        debug!("hot-plug mode");
-        match unsafe { fork() }.expect("fork hot-plug") {
-            ForkResult::Parent { .. } => {
-                kata_agent().expect("kata-agent hot-plug parent");
-            }
-            ForkResult::Child => {
-                self.handle_hot_plug_events().expect("hot-plug events");
-            }
-        }
-    }
-
-    fn handle_hot_plug_events(&mut self) -> Result<()> {
-        let (tx, rx) = mpsc::channel::<&str>();
-        ndev::udev(tx.clone());
-        kmsg::watch_for_pattern("NVRM: Attempting to remove device", tx.clone());
-        for ev in rx {
-            debug!("event: {ev}");
-            match ev {
-                "hot-plug" => {
-                    self.get_nvidia_devices(None)?;
-                    self.setup_gpu();
-                }
-                "hot-unplug" => {
-                    self.manage_daemons(Action::Stop)?;
-                    sleep(Duration::from_millis(3000));
-                    self.get_nvidia_devices(None)?;
-                    if !self.nvidia_devices.is_empty() {
-                        self.manage_daemons(Action::Start)?;
-                    }
-                }
-                _ => {}
-            }
-        }
-        Ok(())
-    }
-
     fn setup_gpu(&mut self) {
         must!(self.check_gpu_supported(None));
         #[cfg(feature = "confidential")]
