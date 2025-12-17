5d0e89226224c55bbfd7574ad48e14dc419f84b0
Zvonko Kaiser <zkaiser@nvidia.com>
2025-12-17 20:20:13 +0000

    Fail if we cannot set the RLIMIT for kata-agent
    Signed-off-by: Zvonko Kaiser <zkaiser@nvidia.com>

--- src/kata_agent.rs
+++ src/kata_agent.rs
@@ -11,9 +11,9 @@ pub fn kata_agent() -> Result<()> {
     let nofile = 1024 * 1024; // desired RLIMIT_NOFILE
     setrlimit(Resource::NOFILE, nofile, nofile).context("setrlimit RLIMIT_NOFILE")?;
     fs::write("/proc/self/oom_score_adj", b"-997").context("write /proc/self/oom_score_adj")?;
-    if let Ok(lim) = rlimit::getrlimit(Resource::NOFILE) {
-        debug!("kata-agent RLIMIT_NOFILE: {:?}", lim);
-    }
+    let lim = rlimit::getrlimit(Resource::NOFILE)?;
+
+    debug!("kata-agent RLIMIT_NOFILE: {:?}", lim);
 
     let err = Command::new("/usr/bin/kata-agent").exec();
     Err(anyhow!("exec /usr/bin/kata-agent failed: {err}"))
